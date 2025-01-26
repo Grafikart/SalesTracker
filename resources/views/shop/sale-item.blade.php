8898a619c69176ba11a4e6171e8660cc955b3802
Jonathan <contact@grafikart.fr>
2025-01-26 21:58:46 +0100

    fix: sales style

--- resources/views/shop/sale-item.blade.php
+++ resources/views/shop/sale-item.blade.php
@@ -2,22 +2,22 @@
     id="sale-{{ $sale->id }}"
     hx-swap-oob="outerHTML"
     @class([
-        "grid gap-4 p-4 grid-cols-[1fr_100px_max-content_max-content_40px] text-dark",
+        "grid gap-3 p-4 grid-cols-[90px_50px_1fr_40px_40px] text-dark items-center",
         "line-through *:opacity-40" => $sale->trashed(),
     ])
 >
-    <div class="font-bold text-slate-900 text-dark">
-        {!! str_replace(['(', ')'], ['<span class="text-xs text-gray">(', ')</span>'], $sale->product->name) !!}
+    <div class="font-bold text-dark">
+        {!! str_replace(['(', ')'], ['<br/><span class="text-xs text-gray">', '</span>'], $sale->product->name) !!}
     </div>
-    <div class="font-bold text-red">{{ $sale->product->price / 100 }} €</div>
-    <div class="text-gray">{{ $sale->author }}</div>
+    <div class="font-bold text-right text-red">{{ $sale->product->price / 100 }} €</div>
+    <div class="text-gray text-right whitespace-nowrap overflow-hidden text-ellipsis">{{ $sale->author }}</div>
     <div>{{ $sale->created_at->isoFormat('HH:mm') }}</div>
     @if($sale->trashed())
         <button
             hx-disabled-elt="this"
             hx-post="{{ route('sale.restore', ['sale' => $sale->id]) }}"
             hx-indicator="#spinner"
-            class="flex items-center border-l-1 border-l-dark justify-center -my-4 -mr-4 disabled:opacity-30">
+            class="flex self-stretch items-center border-l-1 border-l-dark justify-center -my-4 -mr-4 disabled:opacity-30">
             <x-icon.undo class="size-5"/>
         </button>
     @else
@@ -25,7 +25,7 @@
             hx-disabled-elt="this"
             hx-delete="{{ route('sale.show', ['sale' => $sale->id]) }}"
             hx-indicator="#spinner"
-            class="flex items-center border-l-1 border-l-dark justify-center -my-4 -mr-4 disabled:opacity-30">
+            class="flex self-stretch items-center border-l-1 border-l-dark justify-center -my-4 -mr-4 disabled:opacity-30">
             <x-icon.trash class="size-5"/>
         </button>
     @endif
